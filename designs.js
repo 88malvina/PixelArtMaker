f75083cc760541f26afe01bd094469b7697404ae
88malvina <35811460+88malvina@users.noreply.github.com>
2018-04-07 18:21:56 +0200

    Create designs.js

---
+++ designs.js
@@ -0,0 +1,34 @@
+const colorPicker = document.querySelector('#colorPicker');
+const submitButton = document.querySelector('input[type=submit]');
+const colorPicked = document.querySelector('input[type=color]');
+
+function makeGrid() {
+
+  const rows = document.getElementById('inputHeight').value;
+
+  const columns = document.getElementById('inputWeight').value;
+      
+  var table = document.getElementById('pixelCanvas');
+  
+
+  for (let i=0; i<rows; i++){
+    const row= document.createElement("tr");
+	  table.appendChild(row);
+  
+	  for (let x=0; x<columns; x++){
+        const column = document.createElement("td");
+	      row.appendChild(column);
+	  }
+  }
+
+};
+
+submitButton.addEventListener('click', function(e){
+  e.preventDefault(); //so it doesn't reload when i click submit
+  makeGrid()
+})
+
+document.getElementById('pixelCanvas').addEventListener("click", function(){
+	
+    this.style.backgroundColor = "red";
+});
